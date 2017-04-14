6d02b146548ad5df6486ea5c7c60cf49c305db22
Ryan Robison <ryan@Ryans-MacBook-Pro.local>
2017-04-14 01:08:03 -0700

    Stopped messing around with maps. I'll come back for optimizations later.

--- 8charpass.py
+++ 8charpass.py
@@ -4,61 +4,21 @@ import copy
 file = open('8charpass.txt', 'w')
 
 # charOptions = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*'
-# charOptions='qQwWeErRtTuUaAzZxXcCvVbBnNmM123456!@#$%^'
-charOptions = '123'
+charOptions='qQwWeErRtTuUaAzZxXcCvVbBnNmM123456!@#$%^'
+# charOptions = '123456'
 
 charOptionsLength = len(charOptions)
 repeatThreshold = 0
-length = 4
-# charMapTemplate = {}
-# for char in charOptions:
-#     charMapTemplate[char] = 0
-# passes = itertools.permutations(charOptions, length)
-# # print(str(len(passes)))
-# count = 0
+length = 3
 
-# for pw in passes:
-#     permu = {}
-#     for char in pw:
-#         permu[char] = char
-#     # print permu
-#     permuStr = ''
-#     for char in permu:
-#         permuStr += char
-#     # print permuStr
-#     count += 1
-#     if (len(permuStr) == length):
-#         permuStr += '\n'
-#         file.write(str(permuStr))
-#         if (count % 1000000 == 0):
-#             # print(str(count) + " permutations")
-#             print(str(count/3100796899200))
+def append(string, times):
+    for char in charOptions:
+        charCount = string.count(char)
+        if (charCount <= repeatThreshold):
+            newString = string + char
+            if (times == length):
+                file.write(newString + '\n')
+            if (times < length):
+                append(newString, times + 1)
 
-# charMap = copy.copy(charMapTemplate)
-# def append(string, charMap, times):
-#     for char in charOptions:
-#         if (charMap[char] <= repeatThreshold):
-#             newString = string + char
-#             if (times == length):
-#                 file.write(newString + '\n')
-#             if (times < length):
-#                 charMap[char] += 1
-#                 append(newString, charMap, times + 1)
-#     charMap = copy.copy(charMapTemplate)
-
-# append('', charMap, 1)
-charSet = {}
-for char in charOptions:
-    charSet[char] = char
-map = {}
-def inception(depth, innerMap):
-    if (depth < length):
-        for char in charOptions:
-            if (char == '1'):
-                stop = 'here'
-            innerMap[char] = inception(depth + 1, copy.deepcopy(charSet))
-    else:
-        return charSet
-
-inception(0, map)
-fin = 'fin'
+append('', 1)
